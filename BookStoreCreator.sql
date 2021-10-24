3d34ce5cedf5e733ac65b1e1b3d4f96f2562cabd
Sajaad Khan <73954633+Sajaadkhan@users.noreply.github.com>
2021-10-24 10:22:02 -0400

    database creation

---
+++ BookStoreCreator.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE book_store;
+USE book_store;
+
+CREATE TABLE BookInventory(
+Book_ID int primary key auto_increment ,
+Book_name varchar(80),
+Stock int
+);
+
+
+CREATE TABLE BookOrders(
+order_id int auto_increment primary key,
+custFirstName varchar(40),
+custLastName varchar(40),
+Book_ID int,
+foreign key(Book_ID) references BookInventory(Book_ID)
+);
---
+++ InsertBookInventory.sql
@@ -0,0 +1,15 @@
+USE book_store;
+
+INSERT INTO bookinventory VALUES(NULL,'PHP 6.0',7),
+(null,'Database',8),
+(null,'Android',4),
+(null,'Google Analytics',10),
+(null,'Photoshop CC',3),
+(null,'HTML5',12),
+(null,'Python',23);
+
+INSERT INTO bookorders VALUES
+(NULL,'Sajaad','Khan',7),
+(null,'Arshdeep','Singh',5),
+(null,'Imran','Khan',5),
+(null,'Sehbaaz','Khan',6);
